5182bb825bb2c0fb70358dacc159761821685e4b
Sean Modd <seanmodd@seans-macbook-pro.local>
2020-12-08 14:12:43 -0800

    done

---
+++ setup.js
@@ -0,0 +1,14 @@
+const sqlite3 = require("sqlite3").verbose();
+let testdb = "testdb";
+let db = new sqlite3.Database(testdb, (err) => {
+  if (err) {
+    return console.error(err.message);
+  }
+  console.log("Connected to the SQlite database.");
+});
+db.close((err) => {
+  if (err) {
+    return console.error(err.message);
+  }
+  console.log("Close the database connection.");
+});
